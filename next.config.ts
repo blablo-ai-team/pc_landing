840f0f8b2ccd243909d39aa81c2d51c57632a601
Dương <dbui99620@gmail.com>
2025-07-01 08:49:58 +0700

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  
+  output: 'export', Add commentMore actions
+   images: {
+    unoptimized: true, // <- bắt buộc nếu dùng export
+  },
 };
 
 export default nextConfig;
